7ae30564aa6db5ac50d0a6375fb296636b8040f7
Shivam Gandhi <shivamgandhi@gmail.com>
2022-07-15 20:42:03 -0700

    revert

--- config.js
+++ config.js
@@ -63,6 +63,19 @@ let config = {
       position: "top_left",
     },
     {
+      module: "calendar",
+      header: "US Holidays",
+      position: "top_left",
+      config: {
+        calendars: [
+          {
+            symbol: "calendar-check",
+            url: "webcal://www.calendarlabs.com/ical-calendar/ics/76/US_Holidays.ics",
+          },
+        ],
+      },
+    },
+    {
       module: "compliments",
       position: "lower_third",
     },
